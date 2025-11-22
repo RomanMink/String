0b9efa81d7aa87b4f3ee55d2ac6f618096075024
cpp <roma.minkov1@gmail.com>
2025-11-22 12:33:36 +0100

    cntl c cntl v

--- String.h
+++ String.h
@@ -1,36 +1,23 @@
+
 #pragma once
 #include <cstring>
-
 class String
 {
-	char* str_;
 	size_t length_;
+	char* str_;
 public:
 	String() : String(80) {}
-
 	String(size_t size)
 		: length_{ size }
-		, str_{ new char[length_+1] }
-	{}
-
-	String(const char* str) 
+		, str_{ new char[length_ + 1] }
+	{
+	}
+	String(const char* str)
 		: String(strlen(str))
 	{
-		strcpy_s(str_,length_+1, str);
+		strcpy_s(str_, length_ + 1, str);
 	}
-	~String()
-	{
-		delete[] str_;
-	}
-
-
-	String& setStr()
-	{
-		std::cin >> str_;
-		return *this;
-	}
-	const char* getStr()
-	{
-		return str_;
-	}
+	~String() { delete[] str_; }
+	const char* getStr() { return str_; }
 };
+
